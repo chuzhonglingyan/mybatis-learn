b746e5db5dcbd85df51b274be71f91cadae6ebae
yuntian <944610721@qq.com>
2020-04-24 10:53:09 +0800

    单数据库水平分片

--- sharding-jdbc-demo/db/init.sql
+++ sharding-jdbc-demo/db/init.sql
@@ -1,6 +1,11 @@
+drop database if exists `sharding0`;
+create database `sharding0` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+drop database if exists `sharding1`;
+create database `sharding1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
 CREATE TABLE `t_user0`
 (
-    `id`          bigint(20) NOT NULL,
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
     `name`        varchar(64)         DEFAULT NULL COMMENT '名称',
     `city_id`     int(12)             DEFAULT NULL COMMENT '城市',
     `sex`         tinyint(1)          DEFAULT NULL COMMENT '性别',
@@ -13,7 +18,7 @@ CREATE TABLE `t_user0`
 
 CREATE TABLE `t_user1`
 (
-    `id`          bigint(20) NOT NULL,
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
     `name`        varchar(64)         DEFAULT NULL COMMENT '名称',
     `city_id`     int(12)             DEFAULT NULL COMMENT '城市',
     `sex`         tinyint(1)          DEFAULT NULL COMMENT '性别',
@@ -27,7 +32,7 @@ CREATE TABLE `t_user1`
 
 CREATE TABLE `t_user`
 (
-    `id`          bigint(20) NOT NULL,
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
     `name`        varchar(64)         DEFAULT NULL COMMENT '名称',
     `city_id`     int(12)             DEFAULT NULL COMMENT '城市',
     `sex`         tinyint(1)          DEFAULT NULL COMMENT '性别',
